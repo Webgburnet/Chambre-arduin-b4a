0d23ebe4855a0e4088974d549f53f33e9d511787
Gregoire BURNET <44721323+Webgburnet@users.noreply.github.com>
2018-11-30 08:53:07 +0100

    Add files via upload

--- Chambre_server/Chambre_server.ino
+++ Chambre_server/Chambre_server.ino
@@ -1,10 +1,10 @@
 /* 
-* PROJET DOMOTIQUE CAR TECH INNO
+* PROJET DOMOTIQUE STI2D SIN
 * 
 * Arduino Uno
 * 
 * A0: Grove Capteur Temperature & Humidité
-* A1: Luminosite LDR
+* A1: Grove Luminosite LDR
 * A2: NC
 * A3: NC
 * A4: NC
@@ -27,7 +27,8 @@
 * pin12 : NC
 * pin13 : NC 
 * 
-* I2C : Afficheur LCD RGB
+* I2C : Grove Afficheur LCD RGB
+* I2C : Grove Barometre
 * 
 * Shield Ethernet sans POE
 * Shield Grove
@@ -40,41 +41,55 @@
 #include <rgb_lcd.h>
 #include <SPI.h>
 #include <DHT.h>
+#include <Seeed_BMP280.h>
 
 #define DHTPIN A0
 #define DHTTYPE DHT11   // DHT 11
-#define PIR_MOTION_SENSOR 2
 
+#define RELAIS_MONTEE 4
+#define RELAIS_DESCENTE 5
 #define RELAIS_PRISE 6
 #define RELAIS_LUMIERE 7
 #define RELAIS_CHAUFFAGE 8
 
 DHT dht(DHTPIN, DHTTYPE);
 
+BMP280 bmp280;
+
+//Shield Ethernet sans Aoe Numero 2
 byte mac[]={0x90,0xA2,0xDA,0x0F,0x2C,0x28};
 IPAddress ip_shield(192,168,1,205);
 
 EthernetUDP UDP;
 IPAddress remote = UDP.remoteIP();
+
 rgb_lcd lcd;
 const int colorR = 255;
 const int colorG = 0;
 const int colorB = 0;
 
-byte etat_lumiere=0;
-
 void setup() {
   // put your setup code here, to run once:
   Ethernet.begin(mac,ip_shield);
   UDP.begin(5500);
-  Serial.begin(9600);
-  dht.begin();
   
+  Serial.begin(9600);
+  
+  dht.begin();
+
+  if(!bmp280.init())
+  {
+    Serial.println("Device error!");
+  }
+
+  pinMode( RELAIS_MONTEE , OUTPUT); //Montee du store
+  pinMode( RELAIS_DESCENTE , OUTPUT); //Desecente du store
   pinMode( RELAIS_PRISE , OUTPUT); //Prise
   pinMode( RELAIS_LUMIERE , OUTPUT); //Lumiere
   pinMode( RELAIS_CHAUFFAGE , OUTPUT); //Chauffage
-  pinMode(PIR_MOTION_SENSOR, INPUT);
-  
+
+  digitalWrite(RELAIS_MONTEE , HIGH);
+  digitalWrite(RELAIS_DESCENTE , HIGH);
   digitalWrite(RELAIS_PRISE , HIGH);
   digitalWrite(RELAIS_LUMIERE , HIGH);
   digitalWrite(RELAIS_CHAUFFAGE , HIGH);
@@ -86,94 +101,103 @@ void setup() {
   lcd.print("IP:");
   lcd.setCursor(3,1);
   lcd.print(ip_shield);
-  delay(3000);
+ 
+  delay(1000);
   lcd.clear();
 }
 
 void loop() {
   // Protocole UDP
   int Size=UDP.parsePacket();
-  char message[6];
+  char message[Size];
   String message2;
-  
-
-    
-  lcd.setCursor(0,0);
-  lcd.print("IP:");
-  lcd.setCursor(3,0);
-  lcd.print(ip_shield);
+  int entrer_clavier;
 
   //Variable Capteur temperature et humidité
   float h = dht.readHumidity();
   float t = dht.readTemperature();
   if (isnan(t) || isnan(h)) 
-    {
-        lcd.setCursor(0,1);
-        lcd.print("Pas de capteur DHT");
-        
-        Serial.println("Pas de capteur DHT"); 
-    } 
-    else 
-    {
-        lcd.setCursor(0,1);
-        lcd.print("T=");
-        lcd.setCursor(2,1);
-        lcd.print(t);
-        lcd.setCursor(6,1);
-        lcd.print((char)223);
-        lcd.setCursor(7,1);
-        lcd.print("C H=");
-        lcd.setCursor(11,1);
-        lcd.print(h);
-        lcd.setCursor(15,1);
-        lcd.print("%");
-
-        Serial.print("Humidity: "); 
-        Serial.print(h);
-        Serial.print(" %\t");
-        Serial.print("Temperature: "); 
-        Serial.print(t);
-        Serial.println(" *C");
-    }
-
-    int sensorValue = digitalRead(PIR_MOTION_SENSOR);
-    if(sensorValue == HIGH && etat_lumiere==0)
-    {
-      digitalWrite(RELAIS_LUMIERE , LOW);
-      lcd.clear();
+  {
       lcd.setCursor(0,0);
-      lcd.print("Le message est : ");
-      lcd.setCursor(0,1);
-      lcd.print("Mouvement detecté");
-      delay(2000);
-      digitalWrite(RELAIS_LUMIERE , HIGH);
-      lcd.clear();
-     }
+      lcd.print("Pas de DHT11");
+      
+      Serial.println("Pas de capteur DHT"); 
+  } 
+  else 
+  {
+      lcd.setCursor(0,0);
+      lcd.print("T=");
+      lcd.setCursor(2,0);
+      lcd.print(t);
+      lcd.setCursor(6,0);
+      lcd.print((char)223);
+      lcd.setCursor(7,0);
+      lcd.print("C H=");
+      lcd.setCursor(11,0);
+      lcd.print(h);
+      lcd.setCursor(15,0);
+      lcd.print("%");
+  }
+
+  //Taux de lumiere
+  int Taux_lux = analogRead(A1);
+  lcd.setCursor(0,1);
+  lcd.print("L=");
+  lcd.setCursor(2,1);
+  lcd.print(Taux_lux);
+  lcd.setCursor(5,1);
+  lcd.print("Lux");
+  
+  //Capteur Barometrique
+   float pressure = bmp280.getPressure()/100;
+   float temp=bmp280.getTemperature();
+   float alti=bmp280.calcAltitude(pressure)*100;
+   
+  //Pression atmospherique
+  lcd.setCursor(8,1);
+  lcd.print("P=");
+  lcd.setCursor(10,1);
+  lcd.print(pressure);
+  lcd.setCursor(14,1);
+  lcd.print("Pa");
 
   if(Size>0)
   {
     lcd.clear();
-    UDP.read(message,6);
-    message2=message;
+    UDP.read(message,Size);
+
+    //Convertion du message char UDP en string
+    int cases = 0;
+    while (cases != Size)
+    {
+      message2 = message2 + message[cases];
+      cases = cases+1;
+    }
+  
     lcd.setCursor(0,0);
-    lcd.print("Le message est : ");
+    lcd.print("Msg UDP:");
+    lcd.setCursor(8,0);
+    lcd.print(message2);
     lcd.setCursor(0,1);
-    lcd.print(message);
-    delay(10);
+    lcd.print("Taille Msg UDP:");
+    lcd.setCursor(15,1);
+    lcd.print(Size);
+    
+    delay(500);
     lcd.clear();
-    Serial.println(Size);
-    Serial.println(message);
+
     if(message2=="Acquer")
     {
       lcd.setCursor(0,0);
       lcd.print("Le message est : ");
       lcd.setCursor(0,1);
       lcd.print("Capteur actualisé");
-      delay(2000);
+      
+      delay(1);
       lcd.clear();
-      //char  ReplyBuffer[] = "Capteur DHT11 : ";
+      
       UDP.beginPacket(UDP.remoteIP(), UDP.remotePort());
-      //UDP.write(ReplyBuffer);
+      
       UDP.write("Temperature = ");
       UDP.print(t);
       UDP.write(" C");
@@ -181,6 +205,15 @@ void loop() {
       UDP.write("Humidite = ");
       UDP.print(h);
       UDP.write(" %");
+      UDP.write("\n");
+      UDP.write("Pression = ");
+      UDP.print(pressure);
+      UDP.write(" Pa");
+      UDP.write("\n");
+      UDP.write("Luminosite = ");
+      UDP.print(Taux_lux);
+      UDP.write(" Lux");
+      UDP.write("\n");
       UDP.endPacket();
     }
     else
@@ -190,10 +223,12 @@ void loop() {
       UDP.write(ReplyBuffer);
       UDP.endPacket();
     }
-
   }
 
   /*Liste instruction pour B4A :
+   * Montee : Monter le store
+   * Arrete : Arreter le store
+   * Descen : Desecndre le store
    * ELumiere : Eteindre la lumiere
    * ALumiere : Allumer la lumiere
    * AChauffage : Allumer le chauffage
@@ -201,92 +236,133 @@ void loop() {
    * APrise : Allumer les prises
    * EPrise : Eteindre les prises
    */
+
+   if(message2=="Arrete")
+   {
+    digitalWrite(RELAIS_MONTEE , HIGH);
+    digitalWrite(RELAIS_DESCENTE , HIGH);
+    
+    lcd.clear(); 
+    lcd.setCursor(0,0);
+    lcd.print("Arreter Store : ");
+    lcd.setCursor(0,1);
+    lcd.print("good");
+    
+    delay(1);
+    lcd.clear();
+   }
+
+   if(message2=="Montee")
+   {
+    digitalWrite(RELAIS_MONTEE , LOW);
+    digitalWrite(RELAIS_DESCENTE , HIGH);
+    
+    lcd.clear(); 
+    lcd.setCursor(0,0);
+    lcd.print("Monter Store : ");
+    lcd.setCursor(0,1);
+    lcd.print("good");
+    
+    delay(1);
+    lcd.clear();
+   }
+
+   if(message2=="Descen")
+   {
+    digitalWrite(RELAIS_MONTEE , HIGH);
+    digitalWrite(RELAIS_DESCENTE , LOW);
+
+    lcd.clear(); 
+    lcd.setCursor(0,0);
+    lcd.print("Descendre Store : ");
+    lcd.setCursor(0,1);
+    lcd.print("good");
+    
+    delay(1);
+    lcd.clear();
+   }
    
   if(message2=="APrise")
   {
+    digitalWrite(RELAIS_PRISE , LOW);
+    
     lcd.clear(); 
     lcd.setCursor(0,0);
     lcd.print("Allumer Prise : ");
     lcd.setCursor(0,1);
     lcd.print("good");
-    delay(2000);
-    lcd.clear();
-    digitalWrite(RELAIS_PRISE , LOW);
     
-    Serial.println("Allumer Prise:good");
+    delay(1);
+    lcd.clear();
   }
   
   if(message2=="EPrise")
   {
-    digitalWrite(6 , HIGH);
+    digitalWrite(RELAIS_PRISE , HIGH);
+    
     lcd.clear(); 
     lcd.setCursor(0,0);
     lcd.print("Eteindre Prise :");
     lcd.setCursor(0,1);
     lcd.print("good");
-    delay(2000);
-    lcd.clear();
-    digitalWrite(RELAIS_PRISE , HIGH);
     
-    Serial.println("Eteindre Prise:good");
+    delay(1);
+    lcd.clear();
   }
 
   if(message2=="ALumie")
   {
-    etat_lumiere=1;
     digitalWrite(RELAIS_LUMIERE , LOW);
+    
     lcd.clear(); 
     lcd.setCursor(0,0);
     lcd.print("Allumer Eclairage :");
     lcd.setCursor(0,1);
     lcd.print("Good");
-    delay(2000);
-    lcd.clear();
     
-    Serial.println("Allumer Eclairages:good");
+    delay(1);
+    lcd.clear();
   }
   
   if(message2=="ELumie")
   {
-    etat_lumiere=0;
     digitalWrite(RELAIS_LUMIERE , HIGH);
+    
     lcd.clear(); 
     lcd.setCursor(0,0);
     lcd.print("Eteindre Eclairage : ");
     lcd.setCursor(0,1);
     lcd.print("good");
-    delay(2000);
-    lcd.clear();
     
-    Serial.println("Eteindre Eclairages:good");
+    delay(1);
+    lcd.clear();
   }
 
   if(message2=="AChauf")
   {
     digitalWrite(RELAIS_CHAUFFAGE , LOW);
+    
     lcd.clear(); 
     lcd.setCursor(0,0);
     lcd.print("Allumer Chauffage : ");
     lcd.setCursor(0,1);
     lcd.print("good");
-    delay(2000);
-    lcd.clear();
     
-    Serial.println("Allumer Chauffage:good");
+    delay(1);
+    lcd.clear();   
   }
   
   if(message2=="EChauf")
   {
     digitalWrite(RELAIS_CHAUFFAGE , HIGH);
+    
     lcd.clear(); 
     lcd.setCursor(0,0);
     lcd.print("Eteindre Chauffage:");
     lcd.setCursor(0,1);
     lcd.print("good");
-    delay(2000);
-    lcd.clear();
     
-    Serial.println("Eteindre Chauffage:good");
-  }
-  
+    delay(1);
+    lcd.clear();
+  }  
 }
